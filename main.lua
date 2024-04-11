4be2a58961a099f4f032a39a93dc33d0b64788eb
lukehayes <lukepurplehayes@gmail.com>
2024-04-11 09:28:16 +0100

    Task priority is now passed as an argument into taskmunch:writeTask()

--- main.lua
+++ main.lua
@@ -7,19 +7,23 @@ local task_str = arg[2]
 local priority = arg[3]
 
 taskmunch = TM:new()
-CFG:init()
 
+CFG:init()
 
 if flag == nil then
     print('Run tm -h')
 
 elseif flag == '-p' then
+
     taskmunch:print()
 
 elseif flag == '-xxx' then
 
+    taskmunch:clear()
+
 elseif flag == '-a' then
-    taskmunch:add_task(task_str)
+
+    taskmunch:add_task(task_str, priority)
 
 else
     print(CFG.NO_TASK_TEXT)
